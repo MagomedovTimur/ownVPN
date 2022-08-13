a62038144663b9493af7bd206872eb367082794d
Timur Magomedov <106919947+MagomedovTimur@users.noreply.github.com>
2022-08-13 16:03:04 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt install apache2 apache2-utils python3 php libapache2-mod-php ufw --assume-yes
+apt install apache2 apache2-utils python3 php libapache2-mod-php ufw git --assume-yes
 ufw allow 80/tcp
 ufw allow 443/tcp
 git clone https://github.com/MagomedovTimur/ownVPN
