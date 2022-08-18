94e8a9d306a8e898ee07cfc298b418f6552bf212
Timur Magomedov <106919947+MagomedovTimur@users.noreply.github.com>
2022-08-18 14:32:12 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt install apache2 apache2-utils python3 php libapache2-mod-php ufw git --assume-yes
+apt install apache2 apache2-utils python3 php libapache2-mod-php ufw git strongswan strongswan-pki libcharon-extra-plugins libcharon-extauth-plugins libstrongswan-extra-plugins --assume-yes
 ufw allow 80/tcp
 ufw allow 443/tcp
 git clone https://github.com/MagomedovTimur/ownVPN
